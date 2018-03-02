9b2fcd5a6ecd0dd4f81c67424b446346b3e4ccea
Christophe Garion <tofgarion@runbox.com>
2018-03-02 11:44:27 +0100

    add messages in ILP example

--- ilp_visu_ex.py
+++ ilp_visu_ex.py
@@ -16,25 +16,31 @@ x2_bounds = (0, None)
 x1_gui_bounds = (-1, 16)
 x2_gui_bounds = (-1, 10)
 
+print("Initial problem")
 visu = ILPVisu(A, b, c,
                x1_bounds, x2_bounds,
                x1_gui_bounds, x2_gui_bounds)
+
 input()
 
 # adding some cuts
+print("Adding 2 cuts")
 visu.add_cuts([[1.0, 1.0], [1.0, 0.0]],
               [10.0, 7.0])
 input()
 
 # add one cut
+print("Adding another cut")
 visu.add_cuts([[0.0, 1.0]],
               [6.0])
 input()
 
 # reset cuts and add only one
+print("Removing cuts")
 visu.reset_cuts()
 input()
 
+print("Adding one cut")
 visu.add_cuts([[0.0, 1.0]],
               [6.0])
 input()
